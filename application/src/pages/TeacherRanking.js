fffb4cb3f789c4970cc5bc5e8e7f77b789d7e6d7
ArslanAlimov <aalimov@mail.sfsu.edu>
2022-11-29 22:27:51 -0800

    Added 'random' images to the grid

--- application/src/pages/TeacherRanking.js
+++ application/src/pages/TeacherRanking.js
@@ -2,14 +2,18 @@ import React, { Component, Fragment, useState } from "react"
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search'
-
+import {Grid} from '@mui/material'
+import TeacherCard from "./TeacherCards";
+import { border } from "@mui/system";
 const TeacherRanking = () => {
   return (
-    <><TextField
+    <div>
+
+      <TextField
       id="search_bar"
       type="search"
       placeholder="Last Name, First Name, School Name"
-      style={{ width: "400px" }}
+      style={{display: 'flex', alignItems:'center',justifyContent:'center'}}
       variant="outlined"
       margin="normal"
       onKeyPress={(event) => {
@@ -21,9 +25,56 @@ const TeacherRanking = () => {
           <InputAdornment position="start">
             <SearchIcon />
           </InputAdornment>
-        ), 
+        )
+        , 
       }
-    } /></>
+      
+    } />
+<p style={{display: 'flex', alignItems:'center',justifyContent:'center',color:'red',fontWeight: 'bold' }}>Search for Last Name, First Name, School Name</p>
+<Grid container spacing={24} >
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+  </Grid>
+  <Grid item md={3}>
+  <div> <TeacherCard /> </div>   
+ 
+  </Grid>
+</Grid>
+
+     
+
+    </div>
+
   )
 }
 
